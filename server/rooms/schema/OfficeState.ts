05c6b6bc27d3ffe191c1d5ba4efd27492b3e831a
suyeon woo <woosean999@gmail.com>
2023-01-03 00:54:17 +0900

    refactor. 코드 임시 복구

--- server/rooms/schema/OfficeState.ts
+++ server/rooms/schema/OfficeState.ts
@@ -1,5 +1,11 @@
 import { Schema, ArraySchema, SetSchema, MapSchema, type } from '@colyseus/schema'
-import { IPlayer, IOfficeState, IChatMessage } from '../../../types/IOfficeState'
+import {
+  IPlayer,
+  IOfficeState,
+  IComputer,
+  IWhiteboard,
+  IChatMessage,
+} from '../../../types/IOfficeState'
 
 export class Player extends Schema implements IPlayer {
   @type('string') name = ''
@@ -8,6 +14,15 @@ export class Player extends Schema implements IPlayer {
   @type('string') anim = 'adam_idle_down'
   @type('boolean') readyToConnect = false
   @type('boolean') videoConnected = false
+}
+
+export class Computer extends Schema implements IComputer {
+  @type({ set: 'string' }) connectedUser = new SetSchema<string>()
+}
+
+export class Whiteboard extends Schema implements IWhiteboard {
+  @type('string') roomId = getRoomId()
+  @type({ set: 'string' }) connectedUser = new SetSchema<string>()
 }
 
 export class ChatMessage extends Schema implements IChatMessage {
@@ -19,6 +34,12 @@ export class ChatMessage extends Schema implements IChatMessage {
 export class OfficeState extends Schema implements IOfficeState {
   @type({ map: Player })
   players = new MapSchema<Player>()
+
+  @type({ map: Computer })
+  computers = new MapSchema<Computer>()
+
+  @type({ map: Whiteboard })
+  whiteboards = new MapSchema<Whiteboard>()
 
   @type([ChatMessage])
   chatMessages = new ArraySchema<ChatMessage>()
