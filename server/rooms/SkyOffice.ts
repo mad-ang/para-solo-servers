146266998ffe8f5abe376cb16a7b92d3a1181f46
sunny <dance@kakao.com>
2023-01-04 02:53:50 +0900

    removed: 서버에서 플레이어숫자 관리 제거

---
+++ server/rooms/SkyOffice.ts
@@ -0,0 +1,124 @@
+import bcrypt from 'bcrypt'
+import { Room, Client, ServerError } from 'colyseus'
+import { Dispatcher } from '@colyseus/command'
+import { Player, OfficeState } from './schema/OfficeState'
+import { Message } from '../../types/Messages'
+import { IRoomData } from '../../types/Rooms'
+import { whiteboardRoomIds } from './schema/OfficeState'
+import PlayerUpdateCommand from './commands/PlayerUpdateCommand'
+import PlayerUpdateNameCommand from './commands/PlayerUpdateNameCommand'
+import ChatMessageUpdateCommand from './commands/ChatMessageUpdateCommand'
+
+export class SkyOffice extends Room<OfficeState> {
+  private dispatcher = new Dispatcher(this)
+  private name: string
+  private description: string
+  private password: string | null = null
+
+  async onCreate(options: IRoomData) {
+    const { name, description, password, autoDispose } = options
+    this.name = name
+    this.description = description
+    this.autoDispose = autoDispose
+
+    let hasPassword = false
+    if (password) {
+      const salt = await bcrypt.genSalt(10)
+      this.password = await bcrypt.hash(password, salt)
+      hasPassword = true
+    }
+    this.setMetadata({ name, description, hasPassword })
+
+    this.setState(new OfficeState())
+
+
+    // when receiving updatePlayer message, call the PlayerUpdateCommand
+    this.onMessage(
+      Message.UPDATE_PLAYER,
+      (client, message: { x: number; y: number; anim: string }) => {
+        this.dispatcher.dispatch(new PlayerUpdateCommand(), {
+          client,
+          x: message.x,
+          y: message.y,
+          anim: message.anim,
+        })
+      }
+    )
+
+    // when receiving updatePlayerName message, call the PlayerUpdateNameCommand
+    this.onMessage(Message.UPDATE_PLAYER_NAME, (client, message: { name: string }) => {
+      this.dispatcher.dispatch(new PlayerUpdateNameCommand(), {
+        client,
+        name: message.name,
+      })
+    })
+
+    // when a player is ready to connect, call the PlayerReadyToConnectCommand
+    this.onMessage(Message.READY_TO_CONNECT, (client) => {
+      const player = this.state.players.get(client.sessionId)
+      if (player) player.readyToConnect = true
+    })
+
+    // when a player is ready to connect, call the PlayerReadyToConnectCommand
+    this.onMessage(Message.VIDEO_CONNECTED, (client) => {
+      const player = this.state.players.get(client.sessionId)
+      if (player) player.videoConnected = true
+    })
+
+    // when a player disconnect a stream, broadcast the signal to the other player connected to the stream
+    this.onMessage(Message.DISCONNECT_STREAM, (client, message: { clientId: string }) => {
+      this.clients.forEach((cli) => {
+        if (cli.sessionId === message.clientId) {
+          cli.send(Message.DISCONNECT_STREAM, client.sessionId)
+        }
+      })
+    })
+
+    // when a player send a chat message, update the message array and broadcast to all connected clients except the sender
+    this.onMessage(Message.ADD_CHAT_MESSAGE, (client, message: { content: string }) => {
+      // update the message array (so that players join later can also see the message)
+      this.dispatcher.dispatch(new ChatMessageUpdateCommand(), {
+        client,
+        content: message.content,
+      })
+
+      // broadcast to all currently connected clients except the sender (to render in-game dialog on top of the character)
+      this.broadcast(
+        Message.ADD_CHAT_MESSAGE,
+        { clientId: client.sessionId, content: message.content },
+        { except: client }
+      )
+    })
+  }
+
+  async onAuth(client: Client, options: { password: string | null }) {
+    if (this.password) {
+      const validPassword = await bcrypt.compare(options.password!, this.password)
+      if (!validPassword) {
+        throw new ServerError(403, 'Password is incorrect!')
+      }
+    }
+    return true
+  }
+
+  onJoin(client: Client, options: any) {
+    this.state.players.set(client.sessionId, new Player())
+    client.send(Message.SEND_ROOM_DATA, {
+      id: this.roomId,
+      name: this.name,
+      description: this.description,
+      userCnt: 0,
+    })
+  }
+
+  onLeave(client: Client, consented: boolean) {
+    if (this.state.players.has(client.sessionId)) {
+      this.state.players.delete(client.sessionId)
+    }
+  }
+
+  onDispose() {
+    console.log('room', this.roomId, 'disposing...')
+    this.dispatcher.stop()
+  }
+}
