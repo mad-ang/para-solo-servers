75da55a24ff3aea630a6a9decc6ae7af179a1589
suyeon woo <woosean999@gmail.com>
2023-01-07 17:54:39 +0900

    feat. 회원가입 API 생성

--- server/DB/db.ts
+++ server/DB/db.ts
@@ -1,4 +1,7 @@
 import fs from 'fs'
+import { config } from '../config'
+// const MongoClient = require('mongodb').MongoClient
+const mongoose = require('mongoose')
 
 // export const userDB = JSON.parse(fs.readFileSync(`${__dirname}/rooms.json`, 'utf-8'))
 
@@ -13,4 +16,12 @@ type ROOM_DB_TYPE = {
 //  TODO: 이름 교체
 export const userDB: ROOM_DB_TYPE = {
   rooms: {},
+}
+
+export async function connectDB() {
+  mongoose.set('strictQuery', false)
+  return mongoose.connect(config.db.host, {
+    dbName: 'momstown',
+    useNewUrlParser: true,
+  })
 }
---
+++ server/controller/auth.ts
@@ -0,0 +1,11 @@
+import User from '../model/user'
+import { config } from '../config'
+
+export const createUser = (req, res) => {
+  const user = new User(req.body)
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err })
+    return res.status(200).json({ success: true })
+  })
+}
