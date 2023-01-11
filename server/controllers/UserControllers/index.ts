db743d29fb53a9b9509efc9d655b792c9fb11adc
heesankim <opqryt45@gmail.com>
2023-01-11 14:53:19 +0900

    feat : express-async-errors 핸들링 추가

--- server/controllers/UserControllers/index.ts
+++ server/controllers/UserControllers/index.ts
@@ -7,6 +7,7 @@ import { IUser } from './types';
 import { Request, Response } from 'express';
 import { Token } from './types';
 import User from '../../models/User';
+import 'express-async-errors';
 
 async function hashPassword(user: IUser) {
   const password = user.password;
@@ -23,124 +24,109 @@ async function hashPassword(user: IUser) {
 }
 
 export const signUp = async (req: Request, res: Response) => {
-  try {
-    const user = req.body;
+  const user = req.body;
 
-    if (!user.userId) {
-      return res.status(400).json({
-        status: 400,
-        message: '사용하실 아이디를 입력해주세요.',
-      });
-    }
-    if (!user.password) {
-      return res.status(400).json({
-        status: 400,
-        message: '사용하실 비밀번호를 입력해주세요.',
-      });
-    }
-
-    const foundUser = await User.findOne({ userId: user.userId });
-    if (foundUser) {
-      return res.status(409).json({
-        status: 409,
-        message: '이미 존재하는 아이디입니다.',
-      });
-    }
-
-    user.password = await hashPassword(user);
-    const result = await User.collection.insertOne({
-      userId: user.userId,
-      password: user.password,
-      createdAt: new Date(),
-    });
-    if (!result) {
-      return res.json({ success: false, message: '회원가입 실패' });
-    }
-    return res.status(200).json({
-      status: 200,
-      payload: {
-        userId: user.userId,
-      },
-    });
-  } catch (error) {
-    return res.status(500).json({
-      status: 500,
-      message: `서버 오류: ${error}`,
+  if (!user.userId) {
+    return res.status(400).json({
+      status: 400,
+      message: '사용하실 아이디를 입력해주세요.',
     });
   }
+  if (!user.password) {
+    return res.status(400).json({
+      status: 400,
+      message: '사용하실 비밀번호를 입력해주세요.',
+    });
+  }
+
+  const foundUser = await User.findOne({ userId: user.userId });
+  if (foundUser) {
+    return res.status(409).json({
+      status: 409,
+      message: '이미 존재하는 아이디입니다.',
+    });
+  }
+
+  user.password = await hashPassword(user);
+  const result = await User.collection.insertOne({
+    userId: user.userId,
+    password: user.password,
+    createdAt: new Date(),
+  });
+  if (!result) {
+    return res.json({ success: false, message: '회원가입 실패' });
+  }
+  return res.status(200).json({
+    status: 200,
+    payload: {
+      userId: user.userId,
+    },
+  });
 };
 
 export const login = async (req: Request, res: Response) => {
-  try {
-    // * Validate user input
-    if (!req.body.userId) {
-      return res.status(400).json({
-        status: 400,
-        message: '아이디를 입력해주세요.',
-      });
-    }
-    if (!req.body.password) {
-      return res.status(400).json({
-        status: 400,
-        message: '비밀번호를 입력해주세요.',
-      });
-    }
-    const { userId, password } = req.body;
+  // * Validate user input
+  if (!req.body.userId) {
+    return res.status(400).json({
+      status: 400,
+      message: '아이디를 입력해주세요.',
+    });
+  }
+  if (!req.body.password) {
+    return res.status(400).json({
+      status: 400,
+      message: '비밀번호를 입력해주세요.',
+    });
+  }
+  const { userId, password } = req.body;
 
-    const foundUser = await User.findOne({ userId: userId });
-    if (!foundUser) {
-      return res.status(400).json({
-        status: 400,
-        message: '아이디를 확인해주세요.',
-      });
-    }
+  const foundUser = await User.findOne({ userId: userId });
+  if (!foundUser) {
+    return res.status(400).json({
+      status: 400,
+      message: '아이디를 확인해주세요.',
+    });
+  }
 
-    const isPasswordCorrect = await bcrypt.compare(password, foundUser.password);
-    if (isPasswordCorrect) {
-      const accessToken = jwt.sign(
-        {
-          userId: foundUser.userId,
-          username: foundUser.username,
+  const isPasswordCorrect = await bcrypt.compare(password, foundUser.password);
+  if (isPasswordCorrect) {
+    const accessToken = jwt.sign(
+      {
+        userId: foundUser.userId,
+        username: foundUser.username,
+      },
+      config.jwt.secretKey
+      // {
+      //   expiresIn: config.jwt.expiresInSec,
+      // }
+    );
+
+    const refreshToken = 'refreshToken';
+    await User.collection.updateOne(
+      { userId: foundUser.userId },
+      {
+        $set: {
+          refreshToken: 'refreshToken',
+          lastUpdated: new Date(),
         },
-        config.jwt.secretKey
-        // {
-        //   expiresIn: config.jwt.expiresInSec,
-        // }
-      );
+      }
+    );
 
-      const refreshToken = 'refreshToken';
-      await User.collection.updateOne(
-        { userId: foundUser.userId },
-        {
-          $set: {
-            refreshToken: 'refreshToken',
-            lastUpdated: new Date(),
-          },
-        }
-      );
-
-      res.append('Set-Cookie', `refreshToken=${refreshToken}; Secure; HttpOnly;`);
-      return res.status(200).json({
-        status: 200,
-        payload: {
-          userId: foundUser.userId,
-          accessToken: accessToken,
-        },
-      });
-    } else {
-      return res.status(400).json({
-        status: 400,
-        message: '비밀번호가 올바르지 않습니다.',
-      });
-    }
-  } catch (error) {
-    return res.status(500).json({
-      status: 500,
-      message: `서버 오류: ${error}`,
+    res.append('Set-Cookie', `refreshToken=${refreshToken}; Secure; HttpOnly;`);
+    return res.status(200).json({
+      status: 200,
+      payload: {
+        userId: foundUser.userId,
+        accessToken: accessToken,
+      },
+    });
+  } else {
+    return res.status(400).json({
+      status: 400,
+      message: '비밀번호가 올바르지 않습니다.',
     });
   }
 };
-
 
 const isAuth = async (req: Request, res: Response) => {
   const authHeader = req.get('Authorization');
@@ -159,105 +145,84 @@ const isAuth = async (req: Request, res: Response) => {
 };
 
 export const updateUser = async (req: Request, res: Response) => {
-  try {
-    const decoded = await isAuth(req, res);
-    if (!decoded) return res.status(401).json(AUTH_ERROR);
+  const decoded = await isAuth(req, res);
+  if (!decoded) return res.status(401).json(AUTH_ERROR);
 
-    const previousUserId = decoded.userId;
+  const previousUserId = decoded.userId;
 
-    const newUserData = req.body;
+  const newUserData = req.body;
 
-    if (newUserData.password) {
-      newUserData.password = await hashPassword(newUserData);
-    }
-    newUserData.lastUpdated = new Date();
-
-    User.collection
-      .updateOne(
-        { userId: previousUserId },
-        {
-          $set: newUserData,
-        }
-      )
-      .then(() => {
-        if (newUserData.password) {
-          delete newUserData.password;
-        }
-        return res.status(200).json({
-          status: 200,
-          payload: newUserData,
-        });
-      })
-      .catch(function (error) {
-        return res.status(500).json({
-          status: 404,
-          message: '사용자 정보 변경에 실패했습니다.',
-        });
-      });
-  } catch (error) {
-    return res.status(500).json({
-      status: 500,
-      message: `서버 오류: ${error}`,
-    });
+  if (newUserData.password) {
+    newUserData.password = await hashPassword(newUserData);
   }
+  newUserData.lastUpdated = new Date();
+
+  User.collection
+    .updateOne(
+      { userId: previousUserId },
+      {
+        $set: newUserData,
+      }
+    )
+    .then(() => {
+      if (newUserData.password) {
+        delete newUserData.password;
+      }
+      return res.status(200).json({
+        status: 200,
+        payload: newUserData,
+      });
+    })
+    .catch(function (error) {
+      return res.status(500).json({
+        status: 404,
+        message: '사용자 정보 변경에 실패했습니다.',
+      });
+    });
 };
 
 export const inquireUser = async (req: Request, res: Response) => {
-  try {
-    const decoded = await isAuth(req, res);
-    if (!decoded) return res.status(401).json(AUTH_ERROR);
-    const userId = decoded.userId;
-    const foundUser = await User.findOne({ userId: userId });
+  const decoded = await isAuth(req, res);
+  if (!decoded) return res.status(401).json(AUTH_ERROR);
+  const userId = decoded.userId;
+  const foundUser = await User.findOne({ userId: userId });
 
-    if (foundUser) {
-      return res.status(200).json({
-        status: 200,
-        payload: {
-          userId: foundUser.userId,
-          username: foundUser.username,
-          profileImgUrl: foundUser.profileImgUrl,
-        },
-      });
-    }
-    return res.status(404).json({
-      status: 404,
-      message: '조회에 실패했습니다.',
-    });
-  } catch (error) {
-    return res.status(500).json({
-      status: 500,
-      message: `서버 오류: ${error}`,
+  if (foundUser) {
+    return res.status(200).json({
+      status: 200,
+      payload: {
+        userId: foundUser.userId,
+        username: foundUser.username,
+        profileImgUrl: foundUser.profileImgUrl,
+      },
     });
   }
+  return res.status(404).json({
+    status: 404,
+    message: '조회에 실패했습니다.',
+  });
 };
 
 export const deleteUser = async (req: Request, res: Response) => {
-  try {
-    const decoded = await isAuth(req, res);
-    if (!decoded) return res.status(401).json(AUTH_ERROR);
+  const decoded = await isAuth(req, res);
+  if (!decoded) return res.status(401).json(AUTH_ERROR);
 
-    const previousUserId = decoded.userId;
+  const previousUserId = decoded.userId;
 
-    User.collection
-      .deleteOne({ userId: previousUserId })
-      .then(() => {
-        return res.status(200).json({
-          status: 200,
-          payload: {
-            userId: previousUserId,
-          },
-        });
-      })
-      .catch(function (error) {
-        return res.status(404).json({
-          status: 404,
-          message: '삭제에 실패했습니다.',
-        });
+  User.collection
+    .deleteOne({ userId: previousUserId })
+    .then(() => {
+      return res.status(200).json({
+        status: 200,
+        payload: {
+          userId: previousUserId,
+        },
       });
-  } catch (error) {
-    return res.status(500).json({
-      status: 500,
-      message: `서버 오류: ${error}`,
+    })
+    .catch(function (error) {
+      return res.status(404).json({
+        status: 404,
+        message: '삭제에 실패했습니다.',
+      });
     });
-  }
 };
--- server/routes/auth.ts
+++ server/routes/auth.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import 'express-async-errors';
 import { signUp, login, updateUser, inquireUser, deleteUser } from '../controllers/UserControllers';
 
 const router = express.Router();
@@ -12,5 +13,13 @@ router.patch('/update', updateUser);
 router.get('/me', inquireUser);
 
 router.delete('/delete', deleteUser);
+
+router.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).json({
+    status: 500,
+    message: `서버 오류: ${err}`,
+  });
+});
 
 export default router;
