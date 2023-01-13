d92f53f469c0b988c807777f65dd2554c58e32f1
kiwkim <atthenoon@gmail.com>
2023-01-13 18:50:57 +0900

    feat. chatController

--- server/controllers/ChatControllers/index.ts
+++ server/controllers/ChatControllers/index.ts
@@ -2,13 +2,55 @@ import { config } from '../../envconfig';
 import { Request, Response } from 'express';
 import Chat from '../../models/Chat';
 import { createCollection } from '../../DB/db';
+import { Socket } from 'socket.io';
+import { v4 as uuidV4 } from 'uuid';
 
-export const createChat = () => {
-  //   console.log(343434)
-  //   const chat = new Chat({})
-  //   chat.save()
-};
+const rooms: Record<string, string[]> = {};
+interface IRoomParams {
+  roomId: string;
+  host: string;
+  guest: string;
+}
 const time_diff = 9 * 60 * 60 * 1000;
+
+export const chatHandler = (socket: Socket) => {
+  const createRoom = () => {
+    const roomId = uuidV4();
+    rooms[roomId] = [];
+    socket.emit('room-created', { roomId });
+    console.log('chatroom[', roomId, '] created.');
+  };
+  const joinRoom = ({ roomId, host }: IRoomParams) => {
+    if (rooms[roomId]) {
+      console.log('user Joined the room.', roomId, host);
+      rooms[roomId].push(host);
+      socket.to(roomId).emit('user-joined', { host });
+      socket.join(roomId);
+      socket.emit('get-users', { roomId, participants: rooms[roomId] });
+    }
+
+    socket.on('disconnect', () => {
+      console.log('user left the room', host);
+      // leaveRoom({roomId, host});
+    });
+  };
+
+  const leaveRoom = ({ roomId, host }: IRoomParams) => {
+    rooms[roomId] = rooms[roomId].filter((id) => id !== host);
+    socket.to(roomId).emit('user-disconnected', host);
+  };
+  const startChat = ({ roomId, host }: IRoomParams) => {
+    socket.to(roomId).emit('uesr-started-caht', host);
+  };
+  const stopChat = (roomId: string) => {
+    socket.to(roomId).emit('user-stopped-chat');
+  };
+
+  socket.on("create-room", createRoom);
+  socket.on("join-room", joinRoom);
+  socket.on("start-sharing", startChat);
+  socket.on("stop-sharing", stopChat);
+};
 
 export const addChatMessage = (message: {
   senderId: string;
