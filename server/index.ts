904d128be48c3274a6f8b9c0578b5df3386eacb4
suyeon woo <woosean999@gmail.com>
2023-01-26 00:16:58 +0900

    몽땅 다 줌

--- server/index.ts
+++ server/index.ts
@@ -103,7 +103,13 @@ const socketServer = http.createServer(app);
 socketServer.listen(socketPort, () => console.log(`socketServer is running on ${socketPort}`));
 export const io = require('socket.io')(socketServer, {
   cors: {
-    origin: 'https://www.momstown.site',
+    origin: [
+      'https://www.momstown.site',
+      'http://127.0.0.1:5173',
+      'http://127.0.0.1:5174',
+      'http://localhost:5173',
+      'http://localhost:5174',
+    ],
     methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
