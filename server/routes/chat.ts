f20bd4181ba42f48361f4174e1521aba4c33dc0d
heesankim <opqryt45@gmail.com>
2023-01-26 22:40:46 +0900

    feat : paypent.ts 파일 생성

--- server/routes/chat.ts
+++ server/routes/chat.ts
@@ -14,6 +14,7 @@ router.post('/roomList', loaddata);
 router.post('/addFriend', firstdata);
 router.post('/acceptFriend', setfriend);
 router.post('/chargingCoin', chargingCoin);
+
 // router.post('/deleteFriend', deleteFriend);
 
 export default router;
---
+++ server/routes/payment.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import * as paypal from "../paypal";
+
+const router = express.Router();
+
+router.post('/api/orders', async (req, res) => {
+  const order = await paypal.createOrder();
+  res.json(order);
+});
+
+router.post('/api/orders:orderID/Capture', async (req, res) => {
+  const {orderID} = req.params
+  const captureData = await paypal.capturePayment(orderID)
+  res.json(captureData);
+});
+
+// router.post('/paypal:orderID/capture', p);
+
+export default router;
