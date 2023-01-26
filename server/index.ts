a68795af7a7d7a10a525314d5776f8d79b3d8bdf
suyeon woo <woosean999@gmail.com>
2023-01-27 00:16:02 +0900

    allowedOrigins 추가

--- server/index.ts
+++ server/index.ts
@@ -32,6 +32,7 @@ const allowedOrigins = [
   'https://parasolo-so.link',
   'https://www.parasolo-so.link',
   'https://www.momstown.site',
+  'https://www.para-solo.site',
   'http://127.0.0.1:5173',
   'http://127.0.0.1:5174',
   'http://localhost:5173',
