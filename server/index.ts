1b507cb4537a6a6267511d12ebb59b476e6a12c6
suyeon woo <woosean999@gmail.com>
2023-01-26 12:30:53 +0900

    소켓온리 포트 8080 고정

--- server/index.ts
+++ server/index.ts
@@ -21,7 +21,7 @@ import S3 from './s3';
 const mongoose = require('mongoose');
 var cookieParser = require('cookie-parser');
 const port = 3000;
-const socketPort = Number(process.env.SOCKET_PORT || 8080);
+const socketPort = 8080;
 const app = express();
 app.get('/', (req, res) => {
   res.json({ message: `Server is running on ${req.secure ? 'HTTPS' : 'HTTP'}` });
