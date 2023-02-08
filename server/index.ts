62125d777c9b6976db92beb996090ebd967e1145
suyeon woo <woosean999@gmail.com>
2023-02-08 21:28:27 +0900

    feat. cors allow all

--- server/index.ts
+++ server/index.ts
@@ -28,16 +28,17 @@ app.get('/', (req, res) => {
 });
 app.use(cookieParser());
 
-const allowedOrigins = [
-  'https://parasolo-so.link',
-  'https://www.parasolo-so.link',
-  'https://momstown.site',
-  'https://www.momstown.site',
-  'http://127.0.0.1:5173',
-  'http://127.0.0.1:5174',
-  'http://localhost:5173',
-  'http://localhost:5174',
-];
+// const allowedOrigins = [
+//   'https://parasolo-so.link',
+//   'https://www.parasolo-so.link',
+//   'https://momstown.site',
+//   'https://www.momstown.site',
+//   'http://127.0.0.1:5173',
+//   'http://127.0.0.1:5174',
+//   'http://localhost:5173',
+//   'http://localhost:5174',
+// ];
+const allowedOrigins = '*';
 
 const options: cors.CorsOptions = {
   allowedHeaders: [
